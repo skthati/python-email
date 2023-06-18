9c2137d5d252cd7de7dc93f5d53cfc515b2c674b
skthati <97585808+skthati@users.noreply.github.com>
2023-06-18 14:43:32 +1200

    birthday wisher

--- birthday wisher/main-birthday.py
+++ birthday wisher/main-birthday.py
@@ -1,16 +1,40 @@
+
+"""
+Aim:
+    Check if someone has birthday today and send email.
+
+Args:
+    data : pull the data from birthdays.txt
+    now: present date and time
+    day: extract just day value from "now" variable
+
+
+Returns:
+    Sends email via gmail smtp
+"""
+
+
 import datetime as dt
 import pandas as pd
 import random
+import smtplib
 
+# -----Import data from txt file.
 data = pd.read_csv("birthday wisher/birthdays.txt")
 
-
+# Grab today's date and time.
 now = dt.datetime.now()
-
 day = now.day
 month = now.month
-
 month_day = (month, day)
+
+# ------ Email variables -----#
+
+my_email = "pythonmail@sloka.co.nz"
+my_password = "Today@123"
+to_email = "sandeep@thati.org"
+
+# Convert data to dictionary
 
 data_dict = {(data_row['month'], data_row['day']): data_row for (index, data_row) in data.iterrows()}
 
@@ -22,6 +46,14 @@ if month_day in data_dict:
         file_data = file_data.replace("[name]", data_dict[month_day]["name"])
         print(file_data)
 
+    with smtplib.SMTP("smtp.gmail.com") as birthday_email:
+        birthday_email.starttls()
+        birthday_email.login(user=my_email, password=my_password)
+        birthday_email.sendmail(
+            from_addr = my_email,
+            to_addrs = to_email, 
+            msg=f"Subject: Happy Birthday {data_dict[month_day]['name']}!!\n\n{file_data}"
+        )
 
 # for i in data_dict:
 #     if i == month_day:
